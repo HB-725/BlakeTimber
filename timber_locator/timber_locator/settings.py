b56bfcbdb6b656489fc04ccd518b70679766f0ed
HB725 <hyhxc1031@gmail.com>
2025-05-31 14:08:56 +1000

    fix css difference

--- timber_locator/timber_locator/settings.py
+++ timber_locator/timber_locator/settings.py
@@ -180,9 +180,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+# Remove STATICFILES_DIRS since we're using app-level static directories
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Media files
 MEDIA_URL = '/media/'
