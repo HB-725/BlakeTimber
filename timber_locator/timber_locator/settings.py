1703896b1b13d407c0b6418f0c0701434809e752
HB725 <hyhxc1031@gmail.com>
2025-12-23 02:59:23 +1100

    fix staticfile problem

--- timber_locator/timber_locator/settings.py
+++ timber_locator/timber_locator/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.environ.get("STATIC_ROOT") or os.path.join(BASE_DIR, "staticfiles")
 
 # Remove STATICFILES_DIRS since we're using app-level static directories
 # STATICFILES_DIRS = [
