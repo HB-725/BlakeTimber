4cda83bfbc86f30722384ab115839033bb2ff93a
HB725 <hyhxc1031@gmail.com>
2025-12-23 01:59:46 +1100

    Fix startup for Azure: use antenv, PORT, migrate

--- timber_locator/startup.sh
+++ timber_locator/startup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
 
+# Always run from the deployed app folder
+cd /home/site/wwwroot
+
 echo "PORT=$PORT"
 echo "SQLITE_PATH=$SQLITE_PATH"
 echo "MEDIA_ROOT=$MEDIA_ROOT"
 
-python -m pip install -r requirements.txt
+# Activate the Oryx-created virtualenv (name from your logs: antenv)
+source /home/site/wwwroot/antenv/bin/activate
+
+# (Optional) static + migrations
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
+# Start gunicorn on Azure-required port
 gunicorn --bind 0.0.0.0:${PORT} --workers 2 timber_locator.wsgi:application
